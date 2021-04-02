810272037ffbfd8667874a0b53c23fb56f6178e1
Simon Evans <si@si.org>
2021-04-02 10:34:55 +0100

    Update Package.swift
    - Use hypervisor-kit from github.
    
    - Reformat and remove template comments.

--- Package.swift
+++ Package.swift
@@ -6,37 +6,28 @@ import PackageDescription
 let package = Package(
     name: "FakePC",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../HypervisorKit"),
+        .package(url: "https://github.com/spevans/hypervisor-kit", from: "0.0.1"),
         .package(url: "https://github.com/spevans/swift-babab.git", from: "0.0.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FakePC",
             dependencies: [
                 "CFakePC",
-                "HypervisorKit",
+                .product(name: "HypervisorKit", package: "hypervisor-kit"),
                 .product(name: "BABAB", package: "swift-babab"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Logging", package: "swift-log")
+                .product(name: "Logging", package: "swift-log"),
             ],
             exclude: ["bios"],
-            resources: [ .process("Resources") ]
+            resources: [.process("Resources")]
         ),
-        .target(
-            name: "CFakePC",
-            dependencies:[]
-        ),
-        .testTarget(
-            name: "FakePCTests",
-            dependencies: ["FakePC"]),
+        .target(name: "CFakePC", dependencies: []),
+        .testTarget(name: "FakePCTests", dependencies: ["FakePC"]),
     ]
 )
